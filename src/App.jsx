17fe4906b03056e7f0f6e5557a05c5d91408a600
Malik Haziq <malikhaziq153@gmail.com>
2025-08-17 01:56:58 +0500

    add page routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,32 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import GlobalStyles from "./styles/GlobalStyles";
+
+import Dashboard from "./pages/Dashboard";
+import Account from "./pages/Account";
+import Bookings from "./pages/Bookings";
+import Cabins from "./pages/Cabins";
+import Settings from "./pages/Settings";
+import Login from "./pages/Login";
+import Users from "./pages/Users";
+import PageNotFound from "./pages/PageNotFound";
+
 export default function App() {
-  return <div>App</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Navigate replace to="dashboard" />} />
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="account" element={<Account />} />
+          <Route path="bookings" element={<Bookings />} />
+          <Route path="cabins" element={<Cabins />} />
+          <Route path="settings" element={<Settings />} />
+          <Route path="login" element={<Login />} />
+          <Route path="users" element={<Users />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
