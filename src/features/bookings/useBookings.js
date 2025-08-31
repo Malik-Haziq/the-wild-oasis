c06fc36b87c1355165c2c2a44b82b3fb4f571a0e
Malik Haziq <malikhaziq153@gmail.com>
2025-09-01 01:20:07 +0500

    add sorting to bookings page

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -7,18 +7,24 @@ export function useBookings() {
 
   const filterValue = searchParams.get("status");
 
+  // FILTER
   const filter =
     !filterValue || filterValue === "all"
       ? null
       : { field: "status", value: filterValue };
+
+  // SORT
+  const sortByRaw = searchParams.get("sortBy") || "startDate-desc";
+  const [field, direction] = sortByRaw.split("-");
+  const sortBy = { field, direction };
 
   const {
     isLoading,
     data: bookings,
     // isError,
   } = useQuery({
-    queryKey: ["bookings", filter],
-    queryFn: () => getBookings({ filter }),
+    queryKey: ["bookings", filter, sortBy],
+    queryFn: () => getBookings({ filter, sortBy }),
   });
 
   return { isLoading, bookings };
--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -1,14 +1,19 @@
 import { getToday } from "../utils/helpers";
 import supabase from "./supabase";
 
-export async function getBookings({ filter }) {
+export async function getBookings({ filter, sortBy }) {
   let query = supabase
     .from("bookings")
     .select(
       "id, created_at, startDate, endDate, numGuests, numNights, totalPrice, status, cabins(name), guests(fullName, email)"
     );
 
-  if (filter !== null) query = query.eq(filter.field, filter.value);
+  if (filter) query = query.eq(filter.field, filter.value);
+
+  if (sortBy)
+    query = query.order(sortBy.field, {
+      ascending: sortBy.direction === "asc",
+    });
 
   const { data, error } = await query;
 
